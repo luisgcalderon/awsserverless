de14ceb054fca94f318ac32d609b39dc87287028
Luis Calderon <luisg.calderon@devcalcon.com>
2024-12-16 06:01:01 -0600

    role and first lambda function

---
+++ infra/md1/dynamodb.ps1
@@ -0,0 +1,7 @@
+aws dynamodb create-table --table-name 'serverless_workshop_intro' --attribute-definitions AttributeName=_id,AttributeType=S --key-schema AttributeName=_id,KeyType=HASH --billing-mode PAY_PER_REQUEST
+
+aws lambda create-function --function-name 'm1-add-sample-data' --runtime 'python3.13' --role 'arn:aws:iam::239221172306:role/mylambda' --code S3Bucket='dvccserverless',S3Key='m1-add-sample-data.zip' --handler 'lambda_function.lambda_handler' --package-type 'Zip'
+
+aws dynamodb delete-table --table-name 'serverless_workshop_intro'
+
+aws lambda delete-function --function-name 'm1-add-sample-data'
