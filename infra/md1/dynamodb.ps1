4bc8975443ed46bbffbf910332d74410e0468d54
Luis Calderon <luisg.calderon@devcalcon.com>
2024-12-16 06:13:09 -0600

    remove bucket

--- infra/md1/dynamodb.ps1
+++ infra/md1/dynamodb.ps1
@@ -2,6 +2,8 @@ aws dynamodb create-table --table-name 'serverless_workshop_intro' --attribute-d
 
 aws lambda create-function --function-name 'm1-add-sample-data' --runtime 'python3.13' --role 'arn:aws:iam::239221172306:role/mylambda' --code S3Bucket='dvccserverless',S3Key='m1-add-sample-data.zip' --handler 'lambda_function.lambda_handler' --package-type 'Zip'
 
+aws s3 rb 's3://dvccserverless' --force
+
 aws dynamodb delete-table --table-name 'serverless_workshop_intro'
 
 aws lambda delete-function --function-name 'm1-add-sample-data'
